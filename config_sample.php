bb1178490937c5a659773582f265fbf9ad8a1242
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2015-10-29 11:59:01 -0700

    adds config sample

---
+++ config_sample.php
@@ -0,0 +1,10 @@
+<?php
+const API_URL = "https://api.fitbit.com/1/user/";
+const CLIENT_ID  = "";
+const CLIENT_SECRET = "";
+const GRANT_TYPE = "authorization_code";
+const OAUTH_HREF = "https://www.fitbit.com/oauth2/authorize";
+const OAUTH_TOKEN_HREF = "https://api.fitbit.com/oauth2/token";
+const RESPONSE_TYPE = "code";
+const REDIRECT_URI = "http://localhost:8000";
+const SCOPE = "activity"; // space separated list of permissions you need
